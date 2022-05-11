e3bea0af2218490334790f102f79ba59a0200eee
Darko Gjakovski <darkogj@iborn.net>
2022-05-11 22:17:19 +0200

    Clean

---
+++ src/main/java/anemona/api/service/impl/AuthServiceImpl.java
@@ -0,0 +1,28 @@
+package anemona.api.service.impl;
+
+import anemona.api.model.User;
+import anemona.api.model.exceptions.InvalidArgumentsException;
+import anemona.api.model.exceptions.InvalidUserCredentialsException;
+import anemona.api.repository.jpa.UserRepository;
+import anemona.api.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User login(String username, String password) {
+        if (username==null || username.isEmpty() || password==null || password.isEmpty()) {
+            throw new InvalidArgumentsException();
+        }
+        return userRepository.findByUsernameAndPassword(username,
+                password).orElseThrow(InvalidUserCredentialsException::new);
+    }
+
+}
